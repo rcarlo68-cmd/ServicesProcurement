cd86c27aa74acb64b89c83437074055efdd29e0b
Ricardo Cabrera <rcarlo68@gmail.com>
2026-09-04 20:30:50 -0500

    feat: add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import Footer from "@/components/layout/Footer";
 import "./globals.css";
 
@@ -90,6 +91,21 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+
+      {/* Google Analytics 4 */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-1S6E5TWQ63"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-1S6E5TWQ63');
+        `}
+      </Script>
     </html>
   );
 }
